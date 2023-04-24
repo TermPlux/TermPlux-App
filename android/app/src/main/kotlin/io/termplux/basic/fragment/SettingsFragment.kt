0529eb223a61850a9ea593ceca8cbbf3252eceb9
王泳淇 <815099246@qq.com>
2023-04-24 23:04:38 +0800

    commit

--- android/app/src/main/kotlin/io/termplux/basic/fragment/SettingsFragment.kt
+++ android/app/src/main/kotlin/io/termplux/basic/fragment/SettingsFragment.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.LinearLayoutCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.FragmentManager
-import com.google.android.material.appbar.AppBarLayout
-import com.google.android.material.appbar.MaterialToolbar
 import io.termplux.R
 import io.termplux.app.ui.navigation.Screen
 import io.termplux.basic.custom.FragmentScaffold
@@ -25,7 +21,9 @@ class SettingsFragment constructor(
 
     init {
         mSettings = Settings {
-            navigation(Screen.Settings.route)
+            navigation(
+                Screen.Settings.route
+            )
         }
     }
 
@@ -41,43 +39,6 @@ class SettingsFragment constructor(
         savedInstanceState: Bundle?
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
-//        return LinearLayoutCompat(requireActivity()).apply {
-//            orientation = LinearLayoutCompat.VERTICAL
-//            addView(
-//                AppBarLayout(requireActivity()).apply {
-//                    addView(
-//                        MaterialToolbar(requireActivity()).apply {
-//                            title = getString(R.string.menu_settings)
-//                            navigationIcon = ContextCompat.getDrawable(
-//                                requireActivity(),
-//                                R.drawable.baseline_arrow_back_24
-//                            )
-//                            setNavigationOnClickListener {
-//
-//                            }
-//                        },
-//                        AppBarLayout.LayoutParams(
-//                            AppBarLayout.LayoutParams.MATCH_PARENT,
-//                            AppBarLayout.LayoutParams.WRAP_CONTENT
-//                        )
-//                    )
-//                },
-//                LinearLayoutCompat.LayoutParams(
-//                    LinearLayoutCompat.LayoutParams.MATCH_PARENT,
-//                    LinearLayoutCompat.LayoutParams.WRAP_CONTENT
-//                )
-//            )
-//            addView(
-//                FragmentContainerView(requireActivity()).apply {
-//                    id = R.id.settings_container
-//                },
-//                LinearLayoutCompat.LayoutParams(
-//                    LinearLayoutCompat.LayoutParams.MATCH_PARENT,
-//                    LinearLayoutCompat.LayoutParams.MATCH_PARENT
-//                )
-//            )
-//        }
-
         return FragmentScaffold(
             context = requireActivity(),
             view = FragmentContainerView(
