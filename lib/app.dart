09ff8b9d1d1ae8637dd77a1e0cd228c345af6d55
王泳淇 <815099246@qq.com>
2023-04-26 13:38:55 +0800

    commit

--- lib/app.dart
+++ lib/app.dart
@@ -11,17 +11,17 @@ class TermPluxApp extends StatelessWidget {
 
   static const String appName = "TermPlux";
 
-  // /// 由于很多同学说没有跳转动画，这里是因为之前exmaple里面用的是 [PageRouteBuilder]，
-  // /// 其实这里是可以自定义的，和Boost没太多关系，比如我想用类似iOS平台的动画，
-  // /// 那么只需要像下面这样写成 [CupertinoPageRoute] 即可
-  // /// (这里全写成[MaterialPageRoute]也行，这里只不过用[CupertinoPageRoute]举例子)
-  // ///
-  // /// 注意，如果需要push的时候，两个页面都需要动的话，
-  // /// （就是像iOS native那样，在push的时候，前面一个页面也会向左推一段距离）
-  // /// 那么前后两个页面都必须是遵循CupertinoRouteTransitionMixin的路由
-  // /// 简单来说，就两个页面都是CupertinoPageRoute就好
-  // /// 如果用MaterialPageRoute的话同理
-  //
+  /// 由于很多同学说没有跳转动画，这里是因为之前exmaple里面用的是 [PageRouteBuilder]，
+  /// 其实这里是可以自定义的，和Boost没太多关系，比如我想用类似iOS平台的动画，
+  /// 那么只需要像下面这样写成 [CupertinoPageRoute] 即可
+  /// (这里全写成[MaterialPageRoute]也行，这里只不过用[CupertinoPageRoute]举例子)
+  ///
+  /// 注意，如果需要push的时候，两个页面都需要动的话，
+  /// （就是像iOS native那样，在push的时候，前面一个页面也会向左推一段距离）
+  /// 那么前后两个页面都必须是遵循CupertinoRouteTransitionMixin的路由
+  /// 简单来说，就两个页面都是CupertinoPageRoute就好
+  /// 如果用MaterialPageRoute的话同理
+
   // Map<String, FlutterBoostRouteFactory> routerMap = {
   //   'mainPage': (settings, uniqueId) {
   //     return CupertinoPageRoute(
@@ -29,9 +29,7 @@ class TermPluxApp extends StatelessWidget {
   //         builder: (_) {
   //           Map<String, Object> map = settings.arguments as Map<String, Object>;
   //           String data = map['data'] as String;
-  //           return SimplePage(
-  //             data: data,
-  //           );
+  //           return const MyHomePage(title: "title");
   //         });
   //   },
   // };
