cf4fc247659ae0da42ba30d1ba2814f67c5181bb
王泳淇 <815099246@qq.com>
2023-05-12 18:19:29 +0800

    commit

--- android/app/src/main/kotlin/io/termplux/activity/TermPluxFlutter.kt
+++ android/app/src/main/kotlin/io/termplux/activity/TermPluxFlutter.kt
@@ -1,35 +1,5 @@
 package io.termplux.activity
 
 import com.idlefish.flutterboost.containers.FlutterBoostActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugins.GeneratedPluginRegistrant
-import io.termplux.custom.LinkNativeViewFactory
 
-class TermPluxFlutter : FlutterBoostActivity() {
-
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-
-        val registry = flutterEngine.platformViewsController.registry
-        registry.registerViewFactory("android_view", LinkNativeViewFactory())
-
-        val messenger = flutterEngine.dartExecutor.binaryMessenger
-        val channel = MethodChannel(messenger, "termplux_channel")
-        channel.setMethodCallHandler { call, res ->
-            when (call.method) {
-
-                "" -> {}
-
-                else -> {
-                    res.error("error", "error_message", null)
-                }
-            }
-        }
-    }
-
-    companion object {
-
-    }
-}
+class TermPluxFlutter : FlutterBoostActivity()
