2d69e363561ad49482a8be6f18e9cd5aa45e79cd
王泳淇 <815099246@qq.com>
2023-05-20 21:17:19 +0800

    commit

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -6,6 +6,7 @@
 #endif
 
 #include "flutter/generated_plugin_registrant.h"
+#include <bitsdojo_window_linux/bitsdojo_window_plugin.h>
 
 struct _MyApplication {
   GtkApplication parent_instance;
@@ -47,9 +48,10 @@ static void my_application_activate(GApplication* application) {
     gtk_window_set_title(window, "TermPlux");
   }
 
-  gtk_window_set_default_size(window, 1280, 720);
+  auto bdw = bitsdojo_window_from(window);
+  bdw->setCustomFrame(true);
   gtk_widget_show(GTK_WIDGET(window));
-
+  
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
 
