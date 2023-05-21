5acad99c1b21398b23be6361bd581b0dd183a86b
王泳淇 <815099246@qq.com>
2023-05-21 22:58:59 +0800

    commit

--- android/app/src/main/kotlin/io/termplux/fragment/MainFragment.kt
+++ android/app/src/main/kotlin/io/termplux/fragment/MainFragment.kt
@@ -5,9 +5,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.FragmentContainerView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
@@ -28,8 +35,6 @@ class MainFragment : FlutterBoostFragment(), Runnable {
     private lateinit var mFragmentContainerView: FragmentContainerView
     private lateinit var mViewPager2: ViewPager2
 
-    private lateinit var channel: MethodChannel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mContext = requireActivity()
@@ -49,22 +54,21 @@ class MainFragment : FlutterBoostFragment(), Runnable {
             setViewCompositionStrategy(
                 ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
             )
-            setContent {
 
-            }
         }
         // 初始化ViewPager
         mViewPager2 = ViewPager2(mContext).apply {
             orientation = ViewPager2.ORIENTATION_HORIZONTAL
         }
         // 返回新的布局
-        return mViewPager2
+        return mComposeView
     }
 
     override fun run() {
         TODO("Not yet implemented")
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mainAdapter = ViewPager2Adapter(
@@ -75,12 +79,45 @@ class MainFragment : FlutterBoostFragment(), Runnable {
             adapter = mainAdapter
             offscreenPageLimit = mainAdapter.itemCount
         }
+
+
+        mComposeView.apply {
+            setContent {
+
+
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = "test")
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+
+                    AndroidView(
+                        factory = {
+                            mFlutterView
+                        },
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(
+                                paddingValues = innerPadding
+                            ),
+                        update = { view ->
+
+                        }
+                    )
+                }
+
+            }
+        }
     }
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         val messenger = flutterEngine.dartExecutor.binaryMessenger
-        channel = MethodChannel(messenger, "termplux_channel")
+        val channel = MethodChannel(messenger, "termplux_channel")
         channel.setMethodCallHandler { call, res ->
             when (call.method) {
                 "pager" -> {
