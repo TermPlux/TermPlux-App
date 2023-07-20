e6490f3925a4c8363b36eee613521739691330ab
王泳淇 <815099246@qq.com>
2023-07-20 13:03:07 +0800

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,19 +51,26 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(dependencyNotation = project(path = ":flutter"))
+    implementation(dependencyNotation = project(path = ":flutter_boost"))
+
+    implementation(dependencyNotation = "androidx.core:core-ktx:1.10.1")
+    implementation(dependencyNotation = "androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+
+    implementation(dependencyNotation = "androidx.activity:activity-compose:1.7.2")
+
+    implementation(dependencyNotation = platform(notation = "androidx.compose:compose-bom:2023.03.00"))
+    implementation(dependencyNotation = "androidx.compose.ui:ui")
+    implementation(dependencyNotation = "androidx.compose.ui:ui-graphics")
+    implementation(dependencyNotation = "androidx.compose.ui:ui-tooling-preview")
+    implementation(dependencyNotation = "androidx.compose.material3:material3")
+
+
+    testImplementation(dependencyNotation = "junit:junit:4.13.2")
+    androidTestImplementation(dependencyNotation = "androidx.test.ext:junit:1.1.5")
+    androidTestImplementation(dependencyNotation = "androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(dependencyNotation = platform(notation = "androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(dependencyNotation = "androidx.compose.ui:ui-test-junit4")
+    debugImplementation(dependencyNotation = "androidx.compose.ui:ui-tooling")
+    debugImplementation(dependencyNotation = "androidx.compose.ui:ui-test-manifest")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,3 @@
-//// Top-level build file where you can add configuration options common to all sub-projects/modules.
-//plugins {
-//    id("com.android.application") version "8.1.0-rc01" apply false
-//    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-//}
-
 buildscript {
     repositories {
         google()
