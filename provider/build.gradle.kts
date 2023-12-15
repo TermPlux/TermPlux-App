af1756e48070909a718f18063abf6ae756fb7c5c
王泳淇 <815099246@qq.com>
2023-12-15 12:46:49 +0800

    commit

--- provider/build.gradle.kts
+++ provider/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -34,11 +35,4 @@ android {
 
 dependencies {
     implementation(dependencyNotation = "dev.rikka.shizuku:provider:13.1.4")
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
