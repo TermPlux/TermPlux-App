4941137f4c3d0c4313cee4a7f56385eca64ff41d
王泳淇 <815099246@qq.com>
2024-02-24 17:35:31 +0800

    commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:termplux_app/app.dart';
-import 'package:termplux_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
